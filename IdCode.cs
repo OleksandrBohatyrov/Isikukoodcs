df8f93c3e6ab76bbc2500cd90fe4536f63c5ee1f
Oleksandr Bohatyrov <bogatyrev895@gmail.com>
2023-09-11 19:35:00 +0300

    Добавьте файлы проекта.

---
+++ IdCode.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isikukood
+{
+    public class IdCode
+    {
+        private readonly string _idCode;
+
+        public IdCode(string idCode)
+        {
+            _idCode = idCode;
+        }
+
+        public static bool IsValidIdCode(string idCode)
+        {
+            if (idCode.Length != 11)
+                return false;
+
+            foreach (char c in idCode)
+            {
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+
+            int controlNumber = int.Parse(idCode[10].ToString());
+            int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
+            int total = CalculateControlNumberWithWeights(idCode, weights);
+
+            if (total % 11 < 10)
+            {
+                return total % 11 == controlNumber;
+            }
+
+            int[] weights2 = { 3, 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+            total = CalculateControlNumberWithWeights(idCode, weights2);
+
+            if (total % 11 < 10)
+            {
+                return total % 11 == controlNumber;
+            }
+
+            return controlNumber == 0;
+        }
+
+        private static int CalculateControlNumberWithWeights(string idCode, int[] weights)
+        {
+            int total = 0;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                total += int.Parse(idCode[i].ToString()) * weights[i];
+            }
+
+            return total;
+        }
+
+        private int GetGenderNumber()
+        {
+            return Convert.ToInt32(_idCode.Substring(0, 1));
+        }
+
+        private bool IsValidGenderNumber()
+        {
+            int genderNumber = GetGenderNumber();
+            return genderNumber > 0 && genderNumber < 7;
+        }
+
+        private int Get2DigitYear()
+        {
+            return Convert.ToInt32(_idCode.Substring(1, 2));
+        }
+
+        public int GetFullYear()
+        {
+            int genderNumber = GetGenderNumber();
+
+            return 1800 + (genderNumber - 1) / 2 * 100 + Get2DigitYear();
+        }
+
+        private int GetMonth()
+        {
+            return Convert.ToInt32(_idCode.Substring(3, 2));
+        }
+
+        private bool IsValidMonth()
+        {
+            int month = GetMonth();
+            return month > 0 && month < 13;
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        }
+
+        private int GetDay()
+        {
+            return Convert.ToInt32(_idCode.Substring(5, 2));
+        }
+
+        private bool IsValidDay()
+        {
+            int day = GetDay();
+            int month = GetMonth();
+            int maxDays = 31;
+            if (new List<int> { 4, 6, 9, 11 }.Contains(month))
+            {
+                maxDays = 30;
+            }
+            if (month == 2)
+            {
+                if (IsLeapYear(GetFullYear()))
+                {
+                    maxDays = 29;
+                }
+                else
+                {
+                    maxDays = 28;
+                }
+            }
+            return 0 < day && day <= maxDays;
+        }
+
+        private int CalculateControlNumberWithWeights(int[] weights)
+        {
+            int total = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                total += Convert.ToInt32(_idCode.Substring(i, 1)) * weights[i];
+            }
+            return total;
+        }
+
+        private bool IsValidControlNumber()
+        {
+            int controlNumber = Convert.ToInt32(_idCode[^1..]);
+            int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
+            int total = CalculateControlNumberWithWeights(weights);
+            if (total % 11 < 10)
+            {
+                return total % 11 == controlNumber;
+            }
+            int[] weights2 = { 3, 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+            total = CalculateControlNumberWithWeights(weights2);
+            if (total % 11 < 10)
+            {
+                return total % 11 == controlNumber;
+            }
+            return controlNumber == 0;
+        }
+
+        public bool IsValid()
+        {
+            return IsValidLength() && ContainsOnlyNumbers()
+                && IsValidGenderNumber() && IsValidMonth()
+                && IsValidDay()
+                && IsValidControlNumber();
+        }
+
+        public DateOnly GetBirthDate()
+        {
+            int day = GetDay();
+            int month = GetMonth();
+            int year = GetFullYear();
+            return new DateOnly(year, month, day);
+        }
+
+        public string GetGender()
+        {
+            int genderNumber = GetGenderNumber();
+            return genderNumber % 2 == 0 ? "Naised" : "Mehed";
+        }
+
+        private bool IsValidLength()
+        {
+            return _idCode.Length == 11;
+        }
+
+        private bool ContainsOnlyNumbers()
+        {
+            for (int i = 0; i < _idCode.Length; i++)
+            {
+                if (!Char.IsDigit(_idCode[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isikukood
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Sisestage ID-kood (11-kohaline) või väljumiseks \"q\": ");
+                string input = Console.ReadLine().Trim();
+
+                if (input.ToLower() == "q")
+                    break;
+
+                if (IdCode.IsValidIdCode(input))
+                {
+                    IdCode id = new IdCode(input);
+                    Console.WriteLine($"Sünniaasta: {id.GetFullYear()}");
+                    Console.WriteLine($"Sünniaeg: {id.GetBirthDate():dd.MM.yyyy}");
+                    Console.WriteLine($"Paul: {id.GetGender()}");
+                }
+                else
+                {
+                    Console.WriteLine("Vale identifitseerimiskood. Proovige uuesti.");
+                }
+            }
+            //Console.WriteLine(new IdCode("27605030298").GetFullYear());  // 1876
+            //Console.WriteLine(new IdCode("37605030299").GetFullYear());  // 1976
+            //Console.WriteLine(new IdCode("50005200009").GetFullYear());  // 2000
+            //Console.WriteLine(new IdCode("27605030298").GetBirthDate());  // 03.05.1876
+            //Console.WriteLine(new IdCode("37605030299").GetBirthDate());  // 03.05.1976
+            //Console.WriteLine(new IdCode("50005200009").GetBirthDate());  // 20.05.2000
+
+
+            //Console.WriteLine(new IdCode("a").IsValid());  // False
+            //Console.WriteLine(new IdCode("123").IsValid());  // False
+            //Console.WriteLine(new IdCode("37605030299").IsValid());  // True
+            //                                                         // 30th February
+            //Console.WriteLine(new IdCode("37602300299").IsValid());  // False
+            //Console.WriteLine(new IdCode("52002290299").IsValid());  // False
+            //Console.WriteLine(new IdCode("50002290231").IsValid());  // True
+            //Console.WriteLine(new IdCode("30002290231").IsValid());  // False
+
+            //// control number 2nd round
+            //Console.WriteLine(new IdCode("51809170123").IsValid());  // True
+            //Console.WriteLine(new IdCode("39806302730").IsValid());  // True
+
+            //// control number 3rd round
+            //Console.WriteLine(new IdCode("60102031670").IsValid());  // True
+            //Console.WriteLine(new IdCode("39106060750").IsValid());  // True
+        }
+    }
+}
